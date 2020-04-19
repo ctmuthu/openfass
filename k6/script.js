f7f3e8eb957ffe84531b2e5a5f75e8be13fc81c0
MCM <MCM@19H0142.local>
2020-04-19 18:36:36 +0200

    k6 stages

--- k6/script.js
+++ k6/script.js
@@ -2,11 +2,18 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 const data = JSON.parse(open("../terraform/config.json"));
 export let options = {
-    vus: 100,
-    duration: '60s',
+  stages: [
+    { duration: "1m", target: 1, rps: 1 },
+    { duration: "1m", target: 200, rps: 1 },
+    { duration: "1m", target: 500, rps: 1 },
+    { duration: "1m", target: 1000, rps: 1 },
+    { duration: "1m", target: 500, rps: 1 },
+    { duration: "1m", target: 200, rps: 1 },
+    { duration: "1m", target: 1, rps: 1 },
+]
   };
 
 export default function() {
   http.get('http://'.concat(`${data.master_ip}`, ':31112/function/figlet'));
-  sleep(1);
+  sleep(2000);
 }
