f72a891772b25c0298a41fa91437742d26ea7d55
ctmuthu <ctmuthu93@gmail.com>
2020-08-16 12:34:26 +0200

    removed unwanted line

--- k6/k6_run_script.js
+++ k6/k6_run_script.js
@@ -1,6 +1,5 @@
 import { check, sleep } from 'k6';
 import http from 'k6/http';
-const data = JSON.parse(open("../terraform/config.json"));
 
 export default function() {
     var url = 'http://'.concat(`${__ENV.MASTER_IP}`, ':31112/function/',`${__ENV.FUNCTION}`);
