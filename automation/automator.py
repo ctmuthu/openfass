82f68cc7d6f7394da3dd4137327aa763f82196e9
ctmuthu <ctmuthu93@gmail.com>
2020-09-16 12:08:31 +0200

    figlet data final collection

--- automation/automator.py
+++ automation/automator.py
@@ -148,7 +148,7 @@ class Deployment:
         for options in self.instance["test"]["k6"]:
             if(options == "customized"):
                 for m in range(1, self.instance["test"]["k6"][options]["stage"]+1):
-                    m=m*5
+                    m=m*4
                     k6 += " --stage" + " 1m:" + str(m)
             elif(type(self.instance["test"]["k6"][options]) == list):
                 for i in self.instance["test"]["k6"][options]:
